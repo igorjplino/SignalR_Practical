9713563b19dca99df010f08c0b1e0b079866a400
igorjplino <igorjplino@gmail.com>
2019-08-13 10:31:54 -0300

    add new functionalities

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -8,6 +9,28 @@ namespace SignalR.Practical
         public Task SendMessageToAll(string message)
         {
             return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(ex);
         }
     }
 }
